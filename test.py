6761faae161dc702220120def5ab2bf8eb730ed0
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2020-06-02 22:32:05 +0200

    Turned model into SEIRS without vitals statistics.

--- test.py
+++ test.py
@@ -3,41 +3,23 @@ import matplotlib.pyplot as plt
 
 from agent.agent import Agent
 
-# N = 10000
-# init_vals = 1 - 1/N, 1/N, 0, 0
-#
-# alpha = 0.2
-# beta = 1.75
-# gamma = 0.5
-# params = alpha, beta, gamma
-#
-# test_agent = Agent(init_vals, [alpha, beta, gamma], [0.3, 0.3])
-#
-# for i in range(100):
-#     test_agent.next()
-#
-# # print(history)
-# plt.plot(test_agent.comps[:, 0])
-# plt.title(f'Single agent SEIR model ({[alpha, beta, gamma]})')
-# plt.show()
-
 
 def params2str(params):
-    alpha, beta, gamma, rho = params
-    return f"alpha: {alpha}, beta: {beta}, gamma: {gamma}, rho: {rho}"
+    alpha, beta, gamma, delta, rho = params
+    return f"alpha: {alpha}, beta: {beta}, gamma: {gamma}, delta {delta}, rho: {rho}"
 
 
 def seir_model_with_measurements(init_vals, params, t):
     S_0, E_0, I_0, R_0, C_0 = init_vals
     S, E, I, R, C = [S_0], [E_0], [I_0], [R_0], [C_0]
-    alpha, beta, gamma, rho = params
+    alpha, beta, gamma, delta, rho = params
     dt = t[1] - t[0]
 
     for _ in t[1:]:
-        next_S = S[-1] - (rho*beta*S[-1]*I[-1])*dt
+        next_S = S[-1] - (rho*beta*S[-1]*I[-1])*dt + (delta*R[-1])*dt  # Add fraction of recovered compartment.
         next_E = E[-1] + (rho*beta*S[-1]*I[-1] - alpha*E[-1])*dt
         next_I = I[-1] + (alpha*E[-1] - gamma*I[-1])*dt
-        next_R = R[-1] + (gamma*I[-1])*dt
+        next_R = R[-1] + (gamma*I[-1])*dt - (delta*R[-1])*dt  # Remove fraction of recovered compartment.
         next_C = C[-1] + (rho*3)*dt  # Cost!
 
         S.append(next_S)
@@ -50,7 +32,7 @@ def seir_model_with_measurements(init_vals, params, t):
 
 
 # Define parameters
-t_max = 100
+t_max = 200
 dt = .1
 t = np.linspace(0, t_max, int(t_max/dt) + 1)
 print(t)
@@ -60,9 +42,10 @@ init_vals = 1 - 1/N, 1/N, 0, 0, 0
 alpha = 0.2
 beta = 1.75
 gamma = 0.5
+delta = 0.2
 rho = 1
 
-params = alpha, beta, gamma, rho
+params = alpha, beta, gamma, delta, rho
 
 # Run simulation
 results = seir_model_with_measurements(init_vals, params, t)
