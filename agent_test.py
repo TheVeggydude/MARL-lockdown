56383be7a3c516a544f9c0423f6778af558b49f1
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2020-09-14 21:52:20 +0200

    Tweaked `agent_test.py`

--- agent_test.py
+++ agent_test.py
@@ -7,17 +7,17 @@ def params2str(params):
     return f"alpha: {alpha}, beta: {beta}, gamma: {gamma}, delta {delta}, rho: {rho}"
 
 
-a = 0.2
-b = 1.75
-g = 0.5
-d = 0.2
-r = 1
+a = 0.2   # exposure rate
+b = 1.75  # Measure of time to infection once exposed
+g = 0.5   # Rate of recovery
+d = 0     # Rate at which loss of immunity occurs.
+r = 1     # Cost level??
 
 initial_state = State(0.99, 0, 0.01, 0)
 initial_params = Parameters(a, b, d, g, r)
 test = Agent(initial_state, initial_params)
 
-for _ in range(50):
+for _ in range(100):
     test.iterate()
 
 print(test.history())
